03c88f71607d5088f0c2958a2926d33ca74b3e9c
87werner <twerner15@googlemail.com>
2021-05-14 15:07:50 +0100

    dropdown feature added for cart using reduc to hold state

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,38 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {auth} from '../../firebase/firebase.utils.js'
-import { ReactComponent as Logo} from '../../assets/crown.svg'
-import {connect} from 'react-redux'
-import'./header.component.styles.scss'
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser}) => {
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils.js";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { connect } from "react-redux";
+import "./header.component.styles.scss";
+
+const Header = ({ currentUser, hidden }) => {
+  //FOR the drop down menu
+  // const [toggleCart,  setToggleCart] = useState
+  // (false);
+
+  //   let handleClickCart = () => {
+  //  console.log(toggleCart)
+  //     setToggleCart(!toggleCart)
+  //   }
+
   return (
     <div className="header">
-      <Link className='logo-container' to='/' >
-            <Logo className='logo'></Logo>
+      <Link className="logo-container" to="/">
+        <Logo className="logo"></Logo>
       </Link>
-      <div className='options'>
-      <Link className='option' to='/shop' >
+      <div className="options">
+        <Link className="option" to="/shop">
           SHOP
-      </Link>
-      <Link className='option' to='/contact' >
-        CONTACT
-      </Link>
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
 
-      {
-        currentUser ? 
-        <div className="option" onClick={() => auth.signOut()} > SIGN OUT</div>
-         :
-        <Link className="option" to='/signin'> SIGN IN</Link>
-      }
-    
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            {" "}
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            {" "}
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon // for dropdown onClick={() => {handleClickCart()}}
+        />
       </div>
+
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
 //state value come from top level root reducer
-const mapStateToProps = state => ({
- currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 //connect is HOC
 export default connect(mapStateToProps)(Header);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,6 @@
 //action creator function
 import {UserActionTypes} from './user.types'
+
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
     payload: user
